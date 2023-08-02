263bca29af30c8a02fcacf66c3f29080dc8417ae
Kamil <k.fazilov@icloud.com>
2023-08-02 17:20:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+
+let btn = document.getElementById("install");
+
+btn.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вы выбрали товар!");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+});
+
+let username = document.getElementById("telegram-name");
+
+username.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
