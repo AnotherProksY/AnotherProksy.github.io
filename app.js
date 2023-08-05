2df58e637911168385dbfc46793b6b27a259b917
Fazilov Kamil <k.fazilov@icloud.com>
2023-08-05 15:50:33 +0300

    фикс

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
 let tg = window.Telegram.WebApp;
-
-// tg.expand();
-
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
-
-let item = "";
-
-let btn = document.getElementById("install");
-
-btn.addEventListener("click", function () {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали товар!");
-        item = "1";
-        tg.MainButton.show();
-    }
-});
-
-Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData(item);
-});
+tg.expand();
 
 const { first_name, last_name, username } = tg.initDataUnsafe.user;
 
 let usernameSpan = document.getElementById("telegram-name");
-
 usernameSpan.innerText = `@${username}`;
