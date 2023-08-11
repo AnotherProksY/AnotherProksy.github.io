44e06b585bb82126af0274e52b9905a89eb269d6
Fazilov Kamil <k.fazilov@icloud.com>
2023-08-11 18:12:38 +0300

    фикс

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let tg = window.Telegram.WebApp;
 
 const { first_name, last_name, username } = tg.initDataUnsafe.user;
+const userPlatform = tg.platform;
+
+var appleLink = "";
+if (userPlatform == "macos") {
+    appleLink = "https://apps.apple.com/ru/app/outline-secure-internet-access/id1356178125"
+} else {
+    appleLink = "https://apps.apple.com/ru/app/outline-app/id1356177741"
+}
+
+// Set Apple download link
+document.getElementById("apple-link").href = appleLink
 
 let usernameSpan = document.getElementById("telegram-name");
 usernameSpan.innerText = `@${username}`;
